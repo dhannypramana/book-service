2b4310400d4651b596a021f0e5868c8e88d01cf0
Dhanny Adhi Pramana <dhanny.pramana@nostratech.com>
2025-07-25 16:45:32 +0700

    fix(book): eliminate redundant queries by using projection in paginated fetch

--- src/main/java/com/danwsaps/catalog/repository/BookRepository.java
+++ src/main/java/com/danwsaps/catalog/repository/BookRepository.java
@@ -1,12 +1,13 @@
 package com.danwsaps.catalog.repository;
 
 import com.danwsaps.catalog.domain.Book;
+import com.danwsaps.catalog.dto.book.projection.BookListProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, String> {
 
-    Page<Book> findByTitleLikeIgnoreCaseAndDeletedFalse(String title, Pageable pageable);
+    Page<BookListProjection> findByTitleLikeIgnoreCaseAndDeletedFalse(String title, Pageable pageable);
 
 }
